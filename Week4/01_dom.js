abc7a93acfdfdc107c54c7f71dbcfe3a9218d7e6
anmolkr7 <anmolojha952@gmail.com>
2025-02-03 23:18:22 +0530

    DOM Intro

---
+++ Week4/01_dom.js
@@ -0,0 +1,11 @@
+/*
+DOM- Document Object Model
+
+The Document Object Model (DOM) is a programming interface(API) that represents an HTML or 
+XML document as a tree structure made of objects, where each node represents an element, attribute, or 
+piece of text.
+
+When a browser loads an HTML document, it creates a structured representation of the 
+document as a tree. Each HTML element becomes a node in this tree.
+
+*/
