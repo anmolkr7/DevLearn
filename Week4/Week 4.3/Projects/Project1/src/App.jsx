2bcc17106906f0bad63cd67c16bfb78eebae2852
anmolkr7 <anmolojha952@gmail.com>
2025-02-09 18:13:30 +0530

    React Intro

--- Week4/Week 4.3/Projects/Project1/src/App.jsx
+++ Week4/Week 4.3/Projects/Project1/src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
+/*
+useState is a React Hook that allows functional components to have state (previously, 
+only class components had state).
+It helps store and update dynamic data in a React component.
+*/
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+/*
+App is a React functional component that acts as the main entry point of your app.
+In React, components are functions that return JSX (HTML-like syntax).
+The function must return a single JSX element (or a fragment <></>).
+*/
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,4 +26,7 @@ function App() {
   )
 }
 
+//This exports the App component so it can be used in other files 
+// (like main.jsx or index.js).
+//In a Vite project, App is typically imported and rendered in main.jsx:
 export default App
