5f9fc7105baf3106d493dc018c3d560fed68b203
anmolkr7 <anmolojha952@gmail.com>
2025-02-11 20:23:21 +0530

    User routes implementation

--- Week4/Week 4.4/05-mongo/routes/user.js
+++ Week4/Week 4.4/05-mongo/routes/user.js
@@ -1,17 +1,30 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const {User,Course}=require("../db")
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement user signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+    await User.create({
+        username,
+        password
+    })
+    res.json({
+        message:"User Created Successfully"
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async(req, res) => {
     // Implement listing all courses logic
+    const  response=await Course.find({});
+    res.json({
+        courses:response
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
 });
 
