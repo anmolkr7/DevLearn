c4cc81c4992c13501770de0ff52c4e427a6ec745
anmolkr7 <anmolojha952@gmail.com>
2025-02-13 14:15:55 +0530

    Admin routes updated

--- Week4/Week 4.4/04-mongo-with-jwt-auth/routes/admin.js
+++ Week4/Week 4.4/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-const {Admin,Course}=require("../db")
+const {Admin,Course, User}=require("../db")
+const jwt=require("jsonwebtoken");
+const JWT_SECRET = require("../index");
 // Admin Routes
 router.post('/signup', async(req, res) => {
     // Implement admin signup logic
@@ -16,8 +18,26 @@ router.post('/signup', async(req, res) => {
     })
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post('/signin', async(req, res) => {
+    const username=req.body.username;
+    const password=req.body.password;
+    const user=await User.find({
+        username,
+        password
+    })
+    if(user){
+    const token=jwt.sign({
+        username
+    },JWT_SECRET);
+    res.json({
+        token
+    })
+}
+else{
+    res.status(411).json({
+        message:"Wrong username and password"
+    })
+}
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
