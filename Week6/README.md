36112c62eedeb6f1942c98e294aeaf69d0a5cd45
anmolkr7 <anmolojha952@gmail.com>
2025-03-08 19:09:55 +0530

    new commit

--- Week6/README.md
+++ Week6/README.md
@@ -1,4 +1,8 @@
 ## Assignment
 Create a react app that has a
 - Header component that takes a title as prop and renders it inside a div
-- The top level App component renders two headers
+- The top level App component renders two Headers
+
+
+After above things being done
+- Update the last app to allow the user to update the title of the first header with a new title containing Math.random number. You can create a button to implement this
--- Week6/src/App.jsx
+++ Week6/src/App.jsx
@@ -4,12 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("My name is abc")
+  function updateTitle(){
+    setTitle("My name is "+Math.random())
+  }
+  /*
+  A re-render menas that
+  1.React did some work to calculate what all should update in this component
+  2. The component actually got called 
+  3.The inspector shows you a bouding box around the component
 
+  It happens when 
+  1. A state variable that is being used inside a component changes
+  2. A parent component re-render triggers all children re-rendering
+
+  Carefully understand 2. point
+  here, clicking on update title only first header should get re render as its state is changing
+  but react re render second header (i.e Kumar) as well
+
+  You want to minimise the number of re-renders to make a highly optimal react app
+  Solution: React.memo
+  */
   return (
       <div>
-        
+        <button onClick={updateTitle}>Update the title of Header 1</button>
+        <Header title={title}></Header>
+        <Header title={"Kumar"}></Header>
       </div>
+  )
+}
+function Header(props){
+  return(
+    <div>
+      {props.title}
+    </div>
   )
 }
 
