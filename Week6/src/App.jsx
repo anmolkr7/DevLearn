16e62020db7c36a134d3ba4220343b08ecdcbb73
anmolkr7 <anmolojha952@gmail.com>
2025-03-08 23:09:43 +0530

    React memo and WrapperComponents

--- Week6/src/App.jsx
+++ Week6/src/App.jsx
@@ -20,11 +20,26 @@ function App() {
 
   Carefully understand 2. point
   here, clicking on update title only first header should get re render as its state is changing
-  but react re render second header (i.e Kumar) as well
+  but react re renders second header (i.e Kumar) as well
 
   You want to minimise the number of re-renders to make a highly optimal react app
-  Solution: React.memo
+  Solution:  
+  1. Create a new component and push the state down in it and here return the button along with that one 
+  header.
+  Tip:-If there are multiple components that need to use a state variable,then find their Lowest 
+  common ancestor and store it there instead of storing the state in root component(i.e App)
+  See solution:- https://gist.github.com/anmolkr7/c287901f9574296e5c983c11a2bf48e5
+
+  2. React.memo
+  Let's you skip re-rendering a component when its props are unchanged
+  Solution:- https://gist.github.com/anmolkr7/2b3c17ebd62684ebf1a21f5dde672203 must see
   */
+ 
+ /*
+ Use {} whenever you want to embed JavaScript in JSX.
+🔹 Without {}, React treats it as plain text.
+🔹 JSX requires {} for dynamic values, but not for static strings.
+ */
   return (
       <div>
         <button onClick={updateTitle}>Update the title of Header 1</button>
@@ -32,7 +47,12 @@ function App() {
         <Header title={"Kumar"}></Header>
       </div>
   )
+  
 }
+/*
+SEE ABOUT WRAPPER COMPONENTS AND TRY TO RUN IT SOMEWHERE TO VISUALISE
+https://gist.github.com/anmolkr7/3aa2f70b0948e66e4f2adc341090ef30
+*/
 function Header(props){
   return(
     <div>
@@ -42,3 +62,4 @@ function Header(props){
 }
 
 export default App
+
