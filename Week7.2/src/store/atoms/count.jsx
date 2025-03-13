30f41a1d354e059c34043618aa9065f77373481e
anmolkr7 <anmolojha952@gmail.com>
2025-03-13 22:47:05 +0530

    Creatin count atom

--- Week7.2/src/store/atoms/count.jsx
+++ Week7.2/src/store/atoms/count.jsx
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
-
+//Atoms are created using the atom function from Recoil.
+//Two arguments inside an object
 export const countAtom = atom({
     key: "countAtom",
     default: 0
 });
+/*
+key: A unique identifier for the atom (must be unique across all atoms).
+default: The initial value of the atom.
+*/
