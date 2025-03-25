126fa007e2de99752df6c9eaa3a1296678bf70a3
anmolkr7 <anmolojha952@gmail.com>
2025-03-25 21:28:16 +0530

    Practice

--- Week9.2/a.js
+++ Week9.2/a.js
@@ -1,3 +1,56 @@
 "use strict";
-const x = 1; //here we are giving a explicit type to x
-console.log(x);
+/*
+TypeScript provides several basic types to ensure better type safety and readability.
+Here are the most commonly used ones: number, string , boolean, null, undefined,
+array, tuple, object, etc
+*/
+/*
+Problem 1:
+Write a function that greets a user given their first name.
+Argument:firstname
+Logs- Hello {firstname}
+Doesnt return anything
+
+Solution:-
+
+function greet(firstname:string)
+{
+    console.log(`Hello ${firstname}`)
+}
+greet("Anmol")
+
+IMPORTANT
+Q.How is typescript better? you still had to add extra code i.e specifying type
+Ans:- As the codebase gets bigger, a lot of people will call this greet() function, they
+might miss out that functions expects string as an input. For future references , for everyone
+else to understand without looking at the whole code and just by looking at function signature
+they would be able to know that it requires a string as argument
+*/
+/*
+Problem 2: Sum Function
+Write a function that calculates the sum of two numbers and RETURNS IT
+
+we can explicitly specify the return type of function
+functiom sum(args):returntype {
+    //code here
+}
+Even if we dont explicitly specify the return type, it will automatically infer it
+
+Solution:-
+function sum(a:number,b:number) :number
+{
+    return a+b;
+}
+console.log("Sum="+sum(2,3));
+*/
+/*
+Problem 3: Return true or false based on if a user is 18+
+
+Solution:-
+*/
+function user(age) {
+    if (age >= 18)
+        return true;
+    return false;
+}
+console.log(user(22));
