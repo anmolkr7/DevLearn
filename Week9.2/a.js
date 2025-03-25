e915313ede6e5d99b2b97880ff9aea1ef735be6b
anmolkr7 <anmolojha952@gmail.com>
2025-03-25 19:43:56 +0530

    Getting started

---
+++ Week9.2/a.js
@@ -0,0 +1,3 @@
+"use strict";
+const x = 1; //here we are giving a explicit type to x
+console.log(x);
---
+++ Week9.2/a.ts
@@ -0,0 +1,2 @@
+const x: number =1;  //here we are giving a explicit type to x
+console.log(x);
